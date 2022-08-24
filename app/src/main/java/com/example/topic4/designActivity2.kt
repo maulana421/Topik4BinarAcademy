a4233cc0ee63ed2b18909e4eeb234d5e6192dc25
maulana421 <maulanaibnuf@gmaile.com>
2022-08-24 13:40:46 +0700

    first commit

---
+++ app/src/main/java/com/example/topic4/designActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.topic4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class designActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_design2)
+    }
+}
---
+++ app/src/main/java/com/example/topic4/design_satu.kt
@@ -0,0 +1,11 @@
+package com.example.topic4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class design_satu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_design_satu)
+    }
+}
